27982e047e35f75779bf5eb33ee0ad80bba33ea0
AhsokaTano26 <1049109092@qq.com>
2025-07-25 16:07:48 +0800

    更改部分配置

--- nsy/plugins/rssget/__init__.py
+++ nsy/plugins/rssget/__init__.py
@@ -38,7 +38,12 @@ config = get_plugin_config(Config)
 logger.add("data/log/info_log.txt", level="DEBUG",rotation="10 MB")
 logger.add("data/log/error_log.txt", level="ERROR",rotation="10 MB")
 REFRESH_TIME = int(os.getenv('REFRESH_TIME'))
+# 配置项（按需修改）
+RSSHUB_HOST = "http://127.0.0.1:1200"  # RSSHub 实例地址
+TIMEOUT = 30  # 请求超时时间
+MAX_IMAGES = 10  # 最多发送图片数量
 
+scheduler = require("nonebot_plugin_apscheduler").scheduler
 
 async def ignore_group(event: GroupMessageEvent) -> bool:
     """检查是否在忽略的群中"""
@@ -58,12 +63,7 @@ async def User_name_get(id) -> set:
         return sheet1
 
 
-# 配置项（按需修改）
-RSSHUB_HOST = "http://192.168.1.1:1200"  # RSSHub 实例地址
-TIMEOUT = 30  # 请求超时时间
-MAX_IMAGES = 10  # 最多发送图片数量
 
-scheduler = require("nonebot_plugin_apscheduler").scheduler
 
 rss_cmd = on_command("rss",priority=10,block=True,rule=ignore_group)
 
