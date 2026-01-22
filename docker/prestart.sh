7cc8a5a8c6fadd34ada7d7b7b9f49379fa072e93
Xunze Tan <24247472@studentmail.ul.ie>
2026-01-22 14:07:09 +0800

    docker dev build test-3:尝试解决SQLALCHEMY_DATABASE_URL作为系统变量无法被使用的问题

--- docker/prestart.sh
+++ docker/prestart.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 set -e
 
+# Generate .env.prod from environment variables for NoneBot to read
+echo "=== Generating .env.prod ==="
+env | grep -v '^_' | grep -v '^PWD' | grep -v '^SHLVL' | grep -v '^PATH' | grep -v '^HOME' > /app/.env.prod
+echo "ENVIRONMENT=prod" >> /app/.env.prod
+
 echo "=== Running database migrations ==="
 nb orm upgrade
 echo "=== Database migrations completed ==="
